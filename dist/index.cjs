131f446f7fdb27fa536591ef535ee5c65b799785
Debbl <me@aiwan.run>
2023-08-05 19:10:19 +0800

    chore: deps files

---
+++ dist/index.cjs
@@ -0,0 +1,65 @@
+'use strict';
+
+const minimist = require('minimist');
+const prompts = require('prompts');
+const kolorist = require('kolorist');
+const node_child_process = require('node:child_process');
+
+function _interopDefaultCompat (e) { return e && typeof e === 'object' && 'default' in e ? e.default : e; }
+
+const minimist__default = /*#__PURE__*/_interopDefaultCompat(minimist);
+const prompts__default = /*#__PURE__*/_interopDefaultCompat(prompts);
+
+const execCommand = (command) => {
+  try {
+    const result = node_child_process.execSync(command);
+    return result;
+  } catch (e) {
+    console.log(e);
+    process.exit(0);
+  }
+};
+
+minimist__default(process.argv.slice(2));
+const currentRegistry = execCommand("npm config get registry").toString().trim();
+const registries = {
+  npmmirror: {
+    registry: "https://registry.npmmirror.com/"
+  },
+  npm: {
+    registry: "https://registry.npmjs.org/"
+  },
+  yarn: {
+    registry: "https://registry.yarnpkg.com/"
+  }
+};
+const registriesChoices = Object.keys(registries).map(
+  (name) => {
+    const registry = registries[name].registry;
+    return {
+      title: registry === currentRegistry ? kolorist.green(name) : name,
+      description: registry,
+      value: name
+    };
+  }
+);
+async function main() {
+  const response = await prompts__default([
+    {
+      type: "select",
+      name: "registryName",
+      message: "Pick registry",
+      choices: registriesChoices,
+      initial: Object.values(registries).findIndex(
+        (v) => v.registry === currentRegistry
+      ) ?? 0
+    }
+  ]);
+  const registryName = response.registryName;
+  const registry = registries[registryName].registry;
+  execCommand(`npm set registry ${registry}`);
+  console.log("\nDone \u2728");
+  console.log("Current registry is:");
+  console.log(`${kolorist.green(registryName)}: ${kolorist.gray(registry)}`);
+}
+main();
---
+++ dist/index.mjs
@@ -0,0 +1,58 @@
+import minimist from 'minimist';
+import prompts from 'prompts';
+import { green, gray } from 'kolorist';
+import { execSync } from 'node:child_process';
+
+const execCommand = (command) => {
+  try {
+    const result = execSync(command);
+    return result;
+  } catch (e) {
+    console.log(e);
+    process.exit(0);
+  }
+};
+
+minimist(process.argv.slice(2));
+const currentRegistry = execCommand("npm config get registry").toString().trim();
+const registries = {
+  npmmirror: {
+    registry: "https://registry.npmmirror.com/"
+  },
+  npm: {
+    registry: "https://registry.npmjs.org/"
+  },
+  yarn: {
+    registry: "https://registry.yarnpkg.com/"
+  }
+};
+const registriesChoices = Object.keys(registries).map(
+  (name) => {
+    const registry = registries[name].registry;
+    return {
+      title: registry === currentRegistry ? green(name) : name,
+      description: registry,
+      value: name
+    };
+  }
+);
+async function main() {
+  const response = await prompts([
+    {
+      type: "select",
+      name: "registryName",
+      message: "Pick registry",
+      choices: registriesChoices,
+      initial: Object.values(registries).findIndex(
+        (v) => v.registry === currentRegistry
+      ) ?? 0
+    }
+  ]);
+  const registryName = response.registryName;
+  const registry = registries[registryName].registry;
+  execCommand(`npm set registry ${registry}`);
+  console.log("\nDone \u2728");
+  console.log("Current registry is:");
+  console.log(`${green(registryName)}: ${gray(registry)}`);
+}
+main();
