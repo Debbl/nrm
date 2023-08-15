0988c6c38c4b405b0d5fa8fb70c769ac42753c8a
Debbl <me@aiwan.run>
2023-08-15 11:12:48 +0800

    feat: add delete option

---
+++ src/command/delete.ts
@@ -0,0 +1,65 @@
+import { green, red } from "kolorist";
+import prompts from "prompts";
+import { getCurrentRegistry, getRegistries, readFile, writeFile } from "../helper";
+import type { Registries, RegistryChoice } from "../types";
+import { NRMRC_PATH } from "../constants";
+
+async function onDelete() {
+  const currentRegistry = await getCurrentRegistry();
+  const registries = await getRegistries();
+
+  const registriesChoices: RegistryChoice[] = (
+    Object.keys(registries as Registries) as Array<keyof typeof registries>
+  ).map((name) => {
+    const registry = registries[name].registry;
+    return {
+      title: registry === currentRegistry ? green(name) : name,
+      description: registry,
+      value: name,
+    };
+  });
+
+  const index = Object.values(registries).findIndex(
+    (v) => v.registry === currentRegistry,
+  );
+
+  let result: { registryName?: keyof Registries; };
+  try {
+    result = await prompts(
+      [
+        {
+          type: "select",
+          name: "registryName",
+          message: "Pick registry",
+          choices: registriesChoices,
+          initial: index === -1 ? 0 : index,
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(`${red("✖")} Operation cancelled`);
+        },
+      },
+    );
+
+    if (!result.registryName) throw new Error(`${red("✖")} No optioned`);
+  } catch (e: any) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    return;
+  }
+
+  const registryName = result.registryName;
+
+  const nrmrc = await readFile(NRMRC_PATH);
+  delete nrmrc[registryName];
+  await writeFile(NRMRC_PATH, { ...nrmrc });
+
+  // eslint-disable-next-line no-console
+  console.log("\nDone ✨");
+  // eslint-disable-next-line no-console
+  console.log(`Delete registry ${red(registryName)} success`);
+}
+
+export default onDelete;
+
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import minimist from "minimist";
 import onMain from "./command/main";
 import onAdd from "./command/add";
+import onDelete from "./command/delete";
 
 async function main() {
   // eslint-disable-next-line n/prefer-global/process
   const _argv = minimist(process.argv.slice(2));
   if (_argv._.length === 0) {
     await onMain();
-  } else if (_argv._[0] === "add") {
-    await onAdd({
-      name: _argv._[1],
-      registry: _argv._[2],
-    });
+  } else {
+    switch (_argv._[0]) {
+      case "add":
+        await onAdd({
+          name: _argv._[1],
+          registry: _argv._[2],
+        });
+        break;
+      case "del":
+        await onDelete();
+        break;
+      case "delete":
+        await onDelete();
+        break;
+      default:
+    }
   }
 }
 
